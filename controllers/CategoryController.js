a379e3045ccbb8ae1c0d69ae551720030d7860fc
basem <basemkhirat@gmail.com>
2019-04-28 23:50:53 +0200

    update docs

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,5 +1,5 @@
 import Controller from "~/controllers/Controller";
-import Category from '~/models/Category';
+import Category from '~/models/category';
 
 export default class extends Controller {
 
