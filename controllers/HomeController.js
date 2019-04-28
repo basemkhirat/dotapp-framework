35e489500ed5b8b241468e1b65f32fa490eab10b
basem <basemkhirat@gmail.com>
2019-04-29 00:56:56 +0200

    fix

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -11,35 +11,6 @@ export default class extends Controller {
      */
 
     index(req, res, next) {
-
-
         res.ok("online");
-
-        // Cache.get("users", (error, users) => {
-        //
-        //     if (error) return res.error(error);
-        //
-        //     if (users) {
-        //
-        //         res.ok(users);
-        //
-        //     } else {
-        //
-        //         User.find().populate("role").exec((error, users) => {
-        //             if (error) return res.error(error);
-        //
-        //             Cache.set("users", users);
-        //
-        //             res.ok(users);
-        //         })
-        //     }
-        // });
-
-
-        // Category.find().exec((error, rows) => {
-        //     if(error) return res.error(error);
-        //     return res.ok(rows);
-        // });
-
     }
 };
