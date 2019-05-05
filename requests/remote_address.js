4073f6ec900d56d6816077d64ab369611583057f
basem <basemkhirat@gmail.com>
2019-05-05 06:01:23 +0200

    add ip function

--- requests/remote_address.js
+++ requests/remote_address.js
@@ -1,3 +1,7 @@
 export default function () {
-    return this.req.headers['x-forwarded-for'] || this.req.connection.remoteAddress;
+    return this.req.headers['x-real-ip'] ||
+        this.req.headers['x-forwarded-for'] ||
+        this.req.ip || this.req._remoteAddress ||
+        (this.req.connection && this.req.connection.remoteAddress) ||
+        undefined;
 }
