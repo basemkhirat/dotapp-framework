7184316862faa2d03148d8ed9590350fb069fc9c
Mahmoud Rady <mahmoud.r248@gmail.com>
2019-05-16 13:36:32 +0200

    update Filters Media

--- client/src/components/media/FilterMedia.vue
+++ client/src/components/media/FilterMedia.vue
@@ -1,21 +1,21 @@
 <template>
      <div class="filter--media py-4">
           <div class="row align-items-center">
-               <div class="col-6">
-                    <v-select :options="typeOptions" v-model="filterType" label="title" class="select--with--icon v--select--scroll">
+               <div class="col-12 col-md-6">
+                    <v-select :options="typeOptions" v-model="filterType" label="title" class="select--with--icon v--select--scroll my-2 my-md-0">
                          <template slot="option" slot-scope="option">
                               <span :class="option.icon"></span>
                               {{ option.title }}
                          </template>
                     </v-select>
-                    <v-select :options="typeOptionsShow" v-model="filterTypeShow" label="title" class="select--with--icon ml-0 ml-md-4 v--select--scroll">
+                    <v-select :options="typeOptionsShow" v-model="filterTypeShow" label="title" class="select--with--icon ml-0 ml-md-4 v--select--scroll my-2 my-md-0">
                          <template slot="option" slot-scope="option">
                               <span :class="option.icon"></span>
                               {{ option.title }}
                          </template>
                     </v-select>
                </div>
-               <div class="col-6">
+               <div class="col-12 col-sm-6">
                     <div class="search">
                          <b-input placeholder="Search..."
                               type="search"
--- client/vue.config.js
+++ client/vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
     plugins: [
       // new Dotenv()
       new Dotenv({
-        path: './../.env', // load this now instead of the ones in '.env'
+        path: '../.env', // load this now instead of the ones in '.env'
         // safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
         // systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
         // silent: true, // hide any errors
