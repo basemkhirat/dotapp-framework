9afd33a3943b9406f45756ca41c4326c947b5848
Mahmoud Rady <mahmoud.r248@gmail.com>
2019-05-22 17:43:38 +0200

    update Media

--- client/src/components/media/MediaItems.vue
+++ client/src/components/media/MediaItems.vue
@@ -26,115 +26,180 @@
 
         <!-- Modal Quick Edit -->
         <b-modal :canCancel="false" :width="640" :active.sync="modalQuickEdit" scroll="keep"
-            class="modal--custom modal--edit--images">
+            class="modal--custom modal--edit--images modal--lg modal--preview--media">
             <div class="modal--content">
                 <form @submit.prevent="submitForm()">
-                    <h3 class="modal-card-title">Edit Image</h3>
-                    <section class="modal-card-body" v-if="itemSelected">
+                    <div class="row h-100">
+                        <div class="col-12 col-md-8 col-xl-9 media--preview--left">
+                            <div v-if="itemSelected" class="media--content--preview">
 
-                        <!-- Audio -->
-                        <template v-if="itemSelected.type === 'audio'">
-                            <div v-if="itemSelected.provider === 'soundcloud'">
-                                <iframe width="100%" height="166" scrolling="no" 
-                                frameborder="no" 
-                                :src="itemSelected.data.embed">
-                                </iframe>
-                            </div>
-                            <div v-if="itemSelected.provider === 'file'" class="py-4">
-                                <vue-plyr>
-                                    <audio>
-                                        <source :src="itemSelected.url" :type="itemSelected.data.mime"/>
-                                    </audio>
-                                </vue-plyr>
-                            </div>
-                        </template>
-
-                        <!-- Video -->
-                        <template v-if="itemSelected.type === 'video'">
-                            <!-- Video Youtube -->
-                            <div v-if="itemSelected.provider === 'youtube'">
-
-                                <vue-plyr>
-                                    <div class="plyr__video-embed">
-                                         <iframe width="400" 
-                                            class="video--iframe"
-                                            :src="itemSelected.data.embed" 
-                                            frameborder="0" 
-                                            allowfullscreen allowtransparency allow="autoplay"></iframe>
+                                <!-- Audio -->
+                                <template v-if="itemSelected.type === 'audio'">
+                                    <div v-if="itemSelected.provider === 'soundcloud'">
+                                        <iframe width="100%" height="166" scrolling="no" 
+                                        frameborder="no" 
+                                        :src="itemSelected.data.embed">
+                                        </iframe>
                                     </div>
-                                </vue-plyr>
-                            </div>
-                            <!-- Video File -->
-                            <div v-if="itemSelected.provider === 'file'">
-                                <vue-plyr>
-                                    <video poster="poster.png" :src="itemSelected.url">
-                                        <source :src="itemSelected.url" :type="itemSelected.data.mime" size="720">
-                                    </video>
-                                </vue-plyr>
-                            </div>
-                        </template>
+                                    <div v-if="itemSelected.provider === 'file'" class="py-4">
+                                        <vue-plyr>
+                                            <audio>
+                                                <source :src="itemSelected.url" :type="itemSelected.data.mime"/>
+                                            </audio>
+                                        </vue-plyr>
+                                    </div>
+                                </template>
 
-                        <!-- image -->
-                        <template v-if="itemSelected.type === 'image'">
-                            <template v-if="itemSelected.thumbnails">
-                                <img :src="itemSelected.thumbnails.medium" :alt="itemSelected.title">
-                            </template>
-                        </template>
-                        
-                        <div class="content--edit--image mt-3">
-                            <b-field>
-                                <b-input rounded v-model="itemSelected.title" placeholder="Title" type="text"></b-input>
-                            </b-field>
-                            <b-field>
-                                <b-input placeholder="Description" v-model="itemSelected.description" rows="3"
-                                    type="textarea"></b-input>
-                            </b-field>
+                                <!-- Video -->
+                                <template v-if="itemSelected.type === 'video'">
+                                    <!-- Video Youtube -->
+                                    <div v-if="itemSelected.provider === 'youtube'">
 
-                            <div class="field has-addons justify-content-center">
-                                <p class="control flex-fill">
-                                    <a class="button is-rounded w-100" v-clipboard:copy="itemSelected.url"
-                                        v-clipboard:success="onCopy" v-clipboard:error="onError">
-                                        <span class="icon is-small">
-                                            <i class="fas fa-link"></i>
-                                        </span>
-                                        <span>Copy link</span>
-                                    </a>
-                                </p>
-                                <!-- <p class="control flex-fill">
-                                             <a class="button w-100">
-                                                  <span class="icon is-small">
-                                                       <i class="fas fa-crop"></i>
-                                                  </span>
-                                                  <span>Crop</span>
-                                             </a>
-                                        </p> -->
-                                <p class="control flex-fill">
-                                    <a class="button w-100" target="_blank" :href="itemSelected.url">
-                                        <span class="icon is-small">
-                                            <i class="fas fa-external-link-square-alt"></i>
-                                        </span>
-                                        <span>External</span>
-                                    </a>
-                                </p>
-                                <p class="control flex-fill">
-                                    <a class="button is-rounded w-100" @click="confirmCustomDelete()">
-                                        <span class="icon is-small">
-                                            <i class="fas fa-trash"></i>
-                                        </span>
-                                        <span>Delete</span>
-                                    </a>
-                                </p>
+                                        <vue-plyr>
+                                            <div class="plyr__video-embed">
+                                                <iframe width="80%" 
+                                                    class="video--iframe"
+                                                    :src="itemSelected.data.embed" 
+                                                    frameborder="0" 
+                                                    allowfullscreen allowtransparency allow="autoplay"></iframe>
+                                            </div>
+                                        </vue-plyr>
+                                    </div>
+                                    <!-- Video File -->
+                                    <div v-if="itemSelected.provider === 'file'">
+                                        <vue-plyr>
+                                            <video poster="poster.png" :src="itemSelected.url">
+                                                <source :src="itemSelected.url" :type="itemSelected.data.mime" size="720">
+                                            </video>
+                                        </vue-plyr>
+                                    </div>
+                                </template>
+
+                                <!-- Image -->
+                                <template v-if="itemSelected.type === 'image'">
+                                    <template v-if="itemSelected.thumbnails">
+                                        <img :src="itemSelected.thumbnails.max" :alt="itemSelected.title">
+                                    </template>
+                                </template>
+
+                                <!-- Document -->
+                                <template v-if="itemSelected.type === 'document'">
+                                    <!-- <object :data="itemSelected.url" :type="data.mime" width="750px" height="750px"> -->
+                                        <embed :src="itemSelected.url" :type="data.mime">
+                                            <p>This browser does not support this files. Please download the file to view it: 
+                                            <a :href="itemSelected.url">Download PDF</a>.</p>
+                                        </embed>
+                                    <!-- </object> -->
+                                </template>
+                                
                             </div>
                         </div>
-                    </section>
 
-                    <hr class="mt-0">
-                    <div class="d-flex justify-content-center">
-                        <button class="button is-rounded is-light mr-2" type="button"
-                            @click="modalQuickEdit = false">Cancel</button>
-                        <button class="button is-primary is-rounded" type="submit"
-                            :class="{'is-loading': isLoading}">Save</button>
+                        <div class="col-12 col-md-4 col-xl-3 media--preview--right">
+                             <div class="text-left">
+                                <h3 class="modal-card-title text-left">Edit Image</h3>
+                                <div class="createdBy d-flex align-items-center" v-if="itemSelected.user">
+                                    <div class="d-flex align-items-center">
+                                        <div class="avatar mr-2">
+                                            <!-- <img :src="itemSelected.user.photo" v-if="itemSelected.user.photo" :alt="itemSelected.user.first_name"> -->
+                                            <img src="./../../assets/images/user/64.png">
+                                        </div>
+                                       <span class="text-capitalize">  {{itemSelected.user.first_name + ' ' + itemSelected.user.last_name }}</span>
+                                    </div>
+                                    <div class="ml-auto">
+                                        <i class="fas fa-clock mr-2"></i>
+                                        {{itemSelected.created }}
+                                    </div>
+                                </div>
+                                
+                             </div>
+                            <div class="content--edit--image mt-4">
+                                <b-field>
+                                    <b-input rounded v-model="itemSelected.title" placeholder="Title" type="text"></b-input>
+                                </b-field>
+                                <b-field>
+                                    <b-input placeholder="Description" v-model="itemSelected.description" rows="4"
+                                        type="textarea"></b-input>
+                                </b-field>
+
+                                <div class="text-center">
+                                    <a class="button m-2" v-clipboard:copy="itemSelected.url"
+                                        v-clipboard:success="onCopy" v-clipboard:error="onError">
+                                       <div>
+                                            <span class="icon is-small">
+                                                <i class="fas fa-link"></i>
+                                            </span>
+                                       </div>
+                                        <span>Copy link</span>
+                                    </a>
+
+                                    <a class="button m-2" target="_blank" :href="itemSelected.url">
+                                        <div>
+                                            <span class="icon is-small">
+                                                <i class="fas fa-external-link-square-alt"></i>
+                                            </span>
+                                        </div>
+                                        <span>External</span>
+                                    </a>
+
+                                    <a class="button m-2" @click="confirmCustomDelete()">
+                                        <div>
+                                            <span class="icon is-small">
+                                                <i class="fas fa-trash"></i>
+                                            </span>
+                                        </div>
+                                        <span>Delete</span>
+                                    </a>
+
+                                </div>
+
+                                <!-- <div class="field has-addons justify-content-center">
+                                    <p class="control flex-fill">
+                                        <a class="button is-rounded w-100" v-clipboard:copy="itemSelected.url"
+                                            v-clipboard:success="onCopy" v-clipboard:error="onError">
+                                            <span class="icon is-small">
+                                                <i class="fas fa-link"></i>
+                                            </span>
+                                            <span>Copy link</span>
+                                        </a>
+                                        
+                                    </p>
+                                    <p class="control flex-fill">
+                                                <a class="button w-100">
+                                                    <span class="icon is-small">
+                                                        <i class="fas fa-crop"></i>
+                                                    </span>
+                                                    <span>Crop</span>
+                                                </a>
+                                            </p>
+                                    <p class="control flex-fill">
+                                        <a class="button w-100" target="_blank" :href="itemSelected.url">
+                                            <span class="icon is-small">
+                                                <i class="fas fa-external-link-square-alt"></i>
+                                            </span>
+                                            <span>External</span>
+                                        </a>
+                                    </p>
+                                    <p class="control flex-fill">
+                                        <a class="button is-rounded w-100" @click="confirmCustomDelete()">
+                                            <span class="icon is-small">
+                                                <i class="fas fa-trash"></i>
+                                            </span>
+                                            <span>Delete</span>
+                                        </a>
+                                    </p>
+                                </div> -->
+                                 <!-- <hr class="mt-0"> -->
+                                <div class="d-flex justify-content-center action--preview--media">
+                                    <button class="button is-rounded is-light mr-2" type="button" @click="modalQuickEdit = false">Cancel</button>
+                                    <button class="button is-primary is-rounded" type="submit"  :class="{'is-loading': isLoading}">Save</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                   
+
+
                 </form>
             </div>
         </b-modal>
