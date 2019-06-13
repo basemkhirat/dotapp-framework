2b1c1f3bc73121498bdc598d2c387f92129d8b28
basem <basemkhirat@gmail.com>
2019-06-13 13:16:45 +0200

    fix

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -14,13 +14,8 @@ export default class extends Controller {
 
     index(req, res, next) {
 
-        Post.findById("5d00fa90f6d5f65df43fbe60", function (error, post) {
 
-            post.getContent(content => {
-                return res.ok(content);
-            });
-
-        });
+        return res.ok("Hi");
 
 
 
