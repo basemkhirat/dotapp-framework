d4019c38d2446b4c644911c5c2465cd06cbc51f3
basem <basemkhirat@gmail.com>
2019-07-04 12:24:50 +0200

    fix type

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -23,6 +23,10 @@ export default class extends Controller {
             query.where("status", req.param("status"));
         }
 
+        if (req.filled("type")) {
+            query.where("type", req.param("type"));
+        }
+
         if (req.filled("tags")) {
             let tags = Array.isArray(req.param("tags")) ? req.param("tags") : req.param("tags").toArray();
             query.where('tags').in(tags);
