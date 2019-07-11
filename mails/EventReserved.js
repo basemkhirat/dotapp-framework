16fd813ced5e849d4b150b7729999814ab111337
basem <basemkhirat@gmail.com>
2019-07-11 13:42:23 +0200

    fix

--- mails/EventReserved.js
+++ mails/EventReserved.js
@@ -2,15 +2,15 @@ import Template from '~/services/mail/template';
 
 export default class extends Template {
 
-    handle(user, callback) {
+    handle(data, callback) {
 
-        this.render("mails/event_reserved", {user}, (error, data) => {
+        this.render("mails/event_reserved", data, (error, html) => {
             if (error && callback) return callback(error);
 
             this.send({
-                to: user.email,
-                subject: this.req.lang("auth.password_change"),
-                html: data
+                to: data.user.email,
+                subject: "نور | تفاصيل الحجز",
+                html: html
             }, (error, info) => {
                 if (error && callback) return callback(error);
                 if (callback) return callback(null, info);
