81094e9efc2749b83aaf9219683b62476ae1267e
basem <basemkhirat@gmail.com>
2019-07-16 12:40:35 +0200

    fix event date

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -62,7 +62,8 @@ export default class extends Controller {
 
         query.order(req.param("sort_field", "created_at"), req.param("sort_type", "desc"));
 
-        query.populate("all_likes").populate("categories").populate("tags").populate("user").populate("media");
+        query.populate("all_likes").populate("categories").populate("tags").populate("user").populate("media")
+            .populate({path: "place", populate: {path: "parent", populate: {path: "parent"}}});
 
         query.execWithCount((error, result) => {
             if (error) return res.serverError(error);
