f08bb6ffb625d5c3cdb4d38c5e8090531f504c59
basem <basemkhirat@gmail.com>
2019-08-25 15:09:59 +0200

    separate package

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,5 +1,5 @@
 import Controller from './Controller';
-import {google} from 'googleapis';
+// import {google} from 'googleapis';
 
 export default class extends Controller {
 
@@ -11,28 +11,30 @@ export default class extends Controller {
      */
     async index(req, res, next) {
 
+        res.ok("ok");
 
-        let auth = new google.auth.OAuth2(
-            '827628876469-d8mjv567l534qqctlok54uv7von2bp00.apps.googleusercontent.com',
-            '8QrBiybwkIFgyysTFMFtC9ks',
-            'https://your-website.com/google-auth'
-        );
 
-        auth.setCredentials({access_token: 'ya29.GltvBzfNYC9_yTzeKdQg15LePoYLlQMXzEou-Wps4NZkBB9vSnYOZhVHkaMBKI7vcMkKPb6g5WZ05YLKUekzOYQC1tj7LyuIE1nHACXRJ02EoACYAXdN2BA3tdvU'});
+        // let auth = new google.auth.OAuth2(
+        //     '827628876469-d8mjv567l534qqctlok54uv7von2bp00.apps.googleusercontent.com',
+        //     '8QrBiybwkIFgyysTFMFtC9ks',
+        //     'https://your-website.com/google-auth'
+        // );
         //
-        var oauth2 = google.oauth2({
-            auth: auth,
-            version: 'v2'
-        });
-
-        oauth2.userinfo.get(
-            function(err, x) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    res.ok(x);
-                }
-            });
+        // auth.setCredentials({access_token: 'ya29.GltvBzfNYC9_yTzeKdQg15LePoYLlQMXzEou-Wps4NZkBB9vSnYOZhVHkaMBKI7vcMkKPb6g5WZ05YLKUekzOYQC1tj7LyuIE1nHACXRJ02EoACYAXdN2BA3tdvU'});
+        // //
+        // var oauth2 = google.oauth2({
+        //     auth: auth,
+        //     version: 'v2'
+        // });
+        //
+        // oauth2.userinfo.get(
+        //     function(err, x) {
+        //         if (err) {
+        //             console.log(err);
+        //         } else {
+        //             res.ok(x);
+        //         }
+        //     });
 
         // const people = google.people({
         //     version: 'v1',
