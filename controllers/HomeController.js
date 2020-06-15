5560466b8bd3de1328f0a4fd7fa3a6eaaa92f3a4
basemkhirat <basemkhirat@gmail.com>
2020-06-15 03:04:12 +0200

    readme

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,22 +1,12 @@
 import Controller from "dotapp/controller";
-import {Auth} from 'dotapp/services';
 
 export default class extends Controller {
     /**
-     * Show Hello
+     * Say Hello
      * @param req
      * @param res
      */
     async index(req, res) {
-
-        try {
-            return res.ok(Auth.generateToken({name: "ahmed"}));
-        }catch(error) {
-            console.log( typeof error.constructor);
-            return res.serverError(error);
-        }
-
-
-    ///        return res.render("hello");
+        return res.render("hello");
     }
 }
