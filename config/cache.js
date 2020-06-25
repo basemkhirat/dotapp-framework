cc3ffd9a50612d546c70d458290e91fa4a39ac10
Basem Khirat <basemkhirat@gmail.com>
2020-06-26 00:01:49 +0200

    Update cache.js

--- config/cache.js
+++ config/cache.js
@@ -1,4 +1,4 @@
-import path from "path";
+import path from 'path';
 
 export default {
 
@@ -9,26 +9,26 @@ export default {
      * Available engines: memory, file and redis
      */
 
-    default: process.env.CACHE_DRIVER || "memory",
+    default: process.env.CACHE_DRIVER || 'memory',
 
     /**
-     * default "Time To Live" in seconds
+     * default 'Time To Live' in seconds
      */
 
     ttl: process.env.CACHE_TTL || 60,
 
     /**
-     * here you may define all of the cache "engines" for your application
+     * here you may define all of the cache 'engines' for your application
      */
 
     engines: {
 
         file: {
-            path: path.join(process.cwd(), "/storage/cache")
+            path: path.join(process.cwd(), '/storage/cache')
         },
 
         redis: {
-            host: process.env.REDIS_HOST || "127.0.0.1",
+            host: process.env.REDIS_HOST || '127.0.0.1',
             port: process.env.REDIS_PORT || 6379,
             database: process.env.REDIS_DB || 0,
             prefix: process.env.REDIS_PREFIX || "store",
